822d82f0ea1c66d1c8b8395b98ec8f07cf91912c
Rafayel Sahakyan <sahakyan.raphayel@gmail.com>
2021-06-12 12:24:57 +0400

    go app

---
+++ application/main.go
@@ -0,0 +1,16 @@
+package main
+
+import {
+    "fmt"
+    "log"
+    "net/http"
+}
+
+func IndexServer(x http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Automate go server")
+}
+
+func main() {
+    handler := http.HandlerFunc(IndexServer)
+    log.Fatal(http.ListenAndServer(":8080", handler))
+}
